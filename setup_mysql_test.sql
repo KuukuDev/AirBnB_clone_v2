d2c1f5c1d28e1c357b6756e874b6ea8310329e19
ghjimchris <jimrajchris@gmail.com>
2023-06-22 04:04:43 +0000

    Sql setup

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,5 +1,6 @@
 -- prepares a MySQL server for the project
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+--Creating new user named: hbnb_dev with all priviledges
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 GRANT SELECT ON  performance_schema.* TO 'hbnb_test'@'localhost';
